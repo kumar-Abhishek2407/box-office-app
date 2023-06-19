8cfa53445df4d3f42cd56a94ae544c05ad289faa
Abhishek Kumar <abhishek24072000@gmail.com>
2023-06-19 14:07:55 +0530

    created useStarredShows custom hook. Place useStarredShows hook logic in a seperate file, tell user wheather show is starred or not

--- src/components/shows/ShowCard.jsx
+++ src/components/shows/ShowCard.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-const ShowCard = ({ name, image, id, summary, onStarMeClick }) => {
+const ShowCard = ({ name, image, id, summary, onStarMeClick, isStarred }) => {
   const summaryStripped = summary
     ? summary.split(' ').slice(0, 10).join(' ').replace(/<.+?>/g, '')
     : 'No description';
@@ -15,7 +15,7 @@ const ShowCard = ({ name, image, id, summary, onStarMeClick }) => {
           Read More
         </a>
         <button type="button" onClick={() => onStarMeClick(id)}>
-          Star me
+          {isStarred ? 'Unstar me' : 'Star me'}
         </button>
       </div>
     </div>
