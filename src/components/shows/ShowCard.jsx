2b3545ff92b3e4055702f38dda06e6a7dadd7f07
Abhishek Kumar <abhishek24072000@gmail.com>
2023-06-21 11:40:38 +0530

    added fade-in animation to FlexGrid. Added scale animation to StarBtn

--- src/components/shows/ShowCard.jsx
+++ src/components/shows/ShowCard.jsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import styled from 'styled-components';
 import { SearchCard, SearchImgWrapper } from '../common/SearchCard';
 import { StarIcon } from '../common/StarIcon';
@@ -5,18 +6,36 @@ const ShowCard = ({ name, image, id, summary, onStarMeClick, isStarred }) => {
   const summaryStripped = summary
     ? summary.split(' ').slice(0, 10).join(' ').replace(/<.+?>/g, '') + '...'
     : 'No description';
+
+  const StarBtnRef = useRef();
+
+  const handleStarClick = () => {
+    onStarMeClick(id);
+
+    const starBtnEl = StarBtnRef.current;
+
+    if (!starBtnEl) return;
+    if (isStarred) {
+      starBtnEl.classList.remove('animate');
+    } else {
+      starBtnEl.classList.add('animate');
+    }
+  };
+
   return (
     <SearchCard>
       <SearchImgWrapper>
         <img src={image} alt="{name}" />
       </SearchImgWrapper>
+
       <h1>{name}</h1>
       <p>{summaryStripped}</p>
+
       <ActionSection>
         <a href={`/show/${id}`} target="_blank" rel="noreferrer">
           Read More
         </a>
-        <StarBtn type="button" onClick={() => onStarMeClick(id)}>
+        <StarBtn ref={StarBtnRef} type="button" onClick={handleStarClick}>
           <StarIcon active={isStarred} />
           {/* {isStarred ? 'Unstar me' : 'Star me'} */}
         </StarBtn>
@@ -51,5 +70,21 @@ const StarBtn = styled.button`
   align-items: center;
   &:hover {
     cursor: pointer;
+  }
+  &.animate {
+    ${StarIcon} {
+      animation: increase 0.75s ease-in forwards;
+      @keyframes increase {
+        0% {
+          transform: scale(1);
+        }
+        50% {
+          transform: scale(3) rotate(45deg);
+        }
+        100% {
+          transform: scale(1);
+        }
+      }
+    }
   }
 `;
